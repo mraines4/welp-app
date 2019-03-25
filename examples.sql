a2610463953667c2b9937136ec63d9ad12ec504c
Matt Raines <mraines4@gmail.com>
2019-03-25 10:34:32 -0400

    adds examples

---
+++ examples.sql
@@ -0,0 +1,19 @@
+-- user profile
+-- 1. get all info for a user by id
+    -- 1a. get only a few fields for public version
+    -- 1b. get all fields for private version
+-- 2. get all favorites for a user by id
+-- 3. get all refviews by that user by id
+
+-- restaurant profile
+-- 1. get all info for a restaurant by id
+-- 2. get all reviews for restaurant by id
+-- 3. get average refview for a restaurant by id
+-- 4. get count of favorites for restaurant by id
+
+-- restaurant search result (restaurants in Atlanta, GA)
+-- 1a. get all matching rows for restaurant by name (case insensitive search)
+    -- 1b. include average review
+    -- 1c. include number of favorites
+-- 2. limit by minimum review
+-- 3. (super bonus) pagination
