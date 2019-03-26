a1440cbbe0060bc5b3338acd6a19fd4fa6c8472c
Matt Raines <mraines4@gmail.com>
2019-03-26 15:11:07 -0400

    uncomments checks

--- test/test.js
+++ test/test.js
@@ -17,49 +17,49 @@ const Review = require('../models/reviews');
 //     });
 // });
 
-// describe('Restauraunt model', () => {
-//     it('should be able to grab an array of restaurants', async () => {
-//         const arrayOfRestaurants = await Restaurant.getAll();
-//         expect(arrayOfRestaurants).to.be.instanceOf(Array);
-//     });
+describe('Restauraunt model', () => {
+    it('should be able to grab an array of restaurants', async () => {
+        const arrayOfRestaurants = await Restaurant.getAll();
+        expect(arrayOfRestaurants).to.be.instanceOf(Array);
+    });
 
-//     it('should be able to retrieve by id', async () => {
-//         const theRestaurant = await Restaurant.getById(2);
-//         expect(theRestaurant).to.be.instanceOf(Restaurant);
-//     });
+    it('should be able to retrieve by id', async () => {
+        const theRestaurant = await Restaurant.getById(2);
+        expect(theRestaurant).to.be.instanceOf(Restaurant);
+    });
 
-    // it('should update the restaurant', async () => {
-    //     const theRestaurant = await Restaurant.getById(2);
-    //     theRestaurant.name = "Chuck E. Cheese";
-    //     await theRestaurant.save()
-    //     const alsoTheRest = await Restaurant.getById(2);
-    //     expect(alsoTheRest.name).to.be.equal('Chuck E. Cheese');
-//     });
-
-
-// });
-
-// describe('Review model', () => {
-//     it('should be able to grab an array of reviews', async () => {
-//         const arrayOfReviews = await Review.getAll();
-//         expect(arrayOfReviews).to.be.instanceOf(Array);
-//     });
-
-//     it('should be able to retrieve by id', async () => {
-//         const theReview = await Review.getById(2);
-//         expect(theReview).to.be.instanceOf(Review);
-//     });
-
-//     it('should update the review', async () => {
-//         const theReview = await Review.getById(2);
-//         theReview.content = "tis crap";
-//         await theReview.save();
-//         const alsoTheReview = await Review.getById(2);
-//         expect(alsoTheReview.content).to.be.equal('tis crap');
-//     });
+    it('should update the restaurant', async () => {
+        const theRestaurant = await Restaurant.getById(2);
+        theRestaurant.name = "Chuck E. Cheese";
+        await theRestaurant.save()
+        const alsoTheRest = await Restaurant.getById(2);
+        expect(alsoTheRest.name).to.be.equal('Chuck E. Cheese');
+    });
 
 
-// });
+});
+
+describe('Review model', () => {
+    it('should be able to grab an array of reviews', async () => {
+        const arrayOfReviews = await Review.getAll();
+        expect(arrayOfReviews).to.be.instanceOf(Array);
+    });
+
+    it('should be able to retrieve by id', async () => {
+        const theReview = await Review.getById(2);
+        expect(theReview).to.be.instanceOf(Review);
+    });
+
+    it('should update the review', async () => {
+        const theReview = await Review.getById(2);
+        theReview.content = "tis crap";
+        await theReview.save();
+        const alsoTheReview = await Review.getById(2);
+        expect(alsoTheReview.content).to.be.equal('tis crap');
+    });
+
+
+});
 
 describe('Favorite model', () => {
     it('should be able to grab an array of favorites', async () => {
@@ -83,30 +83,30 @@ describe('Favorite model', () => {
 
 });
 
-// describe('Users model', () => {
-//     // 😄
-//     it('should be able to retrieve by id', async () => {
-//         const theUser = await User.getById(3);
-//         theUser.should.be.an.instanceOf(User);
-//         // theUser.should.have.length(1);
-//     });
-//     // 😭
-//     it('should error if no user by id', async () => {
-//         const theUser = await User.getById(55);
-//         expect(theUser).to.be.null;
-//         // theUser.should.be.an.instanceOf(User);
-//         // theUser.should.have.length(1);
-//     });
+describe('Users model', () => {
+    // 😄
+    it('should be able to retrieve by id', async () => {
+        const theUser = await User.getById(3);
+        theUser.should.be.an.instanceOf(User);
+        // theUser.should.have.length(1);
+    });
+    // 😭
+    it('should error if no user by id', async () => {
+        const theUser = await User.getById(55);
+        expect(theUser).to.be.null;
+        // theUser.should.be.an.instanceOf(User);
+        // theUser.should.have.length(1);
+    });
 
-//     it('should update the user', async() => {
-//         // grab a user with id 2
-//         const theUser = await User.getById(2);
-//         // update the email
-//         theUser.email = 'new@new.com';
-//         // save the user
-//         await theUser.save()
-//         const alsoTheUser = await User.getById(2);
-//         // expect the eamil to be equal to the new value
-//         expect(alsoTheUser.email).to.equal('new@new.com');
-//     });
-// });
+    it('should update the user', async() => {
+        // grab a user with id 2
+        const theUser = await User.getById(2);
+        // update the email
+        theUser.email = 'new@new.com';
+        // save the user
+        await theUser.save()
+        const alsoTheUser = await User.getById(2);
+        // expect the eamil to be equal to the new value
+        expect(alsoTheUser.email).to.equal('new@new.com');
+    });
+});
