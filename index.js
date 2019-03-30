61940fcdf0d5dc09215c4c67841ceee9326733af
Matt Raines <mraines4@gmail.com>
2019-03-30 15:06:09 -0400

    adds individual user page

--- index.js
+++ index.js
@@ -20,10 +20,25 @@ const server = http.createServer(async (req, res) => {
         const allRestaurants = await Restaurant.getAll();
         const restaurantJSON = JSON.stringify(allRestaurants);
         res.end(restaurantJSON);
-    } else if (req.url === "/users") {
-        const allUsers = await User.getAll();
-        const userJSON = JSON.stringify(allUsers);
-        res.end(userJSON);
+    } else if (req.url.startsWith("/users")) {
+
+        const parts = req.url.split("/");
+        console.log(parts);
+
+        if (parts.length === 2) {
+            const allUsers = await User.getAll();
+            const userJSON = JSON.stringify(allUsers);
+            res.end(userJSON);
+        } else if (parts.length === 3) {
+            const userId = parts[2];
+            const theUser = await User.getById(userId);
+            const userJSON = JSON.stringify(theUser);
+            res.end(userJSON);
+        } else {
+            res.statusCode = 404;
+            res.end('Resource not found.')
+        }
+
     } else {
         res.end(`{
             message: "Thank you for your patronage. Please send diapers."
